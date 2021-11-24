dd636b951a0e7e0cbc7237500314004d212b7ca7
mdjac <71069474+mdjac@users.noreply.github.com>
2021-11-24 16:45:05 +0100

    modified setupTestUsers to include startup data

--- src/main/java/utils/SetupTestUsers.java
+++ src/main/java/utils/SetupTestUsers.java
@@ -3,15 +3,20 @@ package utils;
 
 import entities.Currency;
 import entities.Role;
+import entities.Stock;
+import entities.Transaction;
 import entities.User;
+import java.io.IOException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 public class SetupTestUsers {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 
+    //Fetches currencies and populates to DB.
+    utils.Utility.populateCurrency();
     EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
     EntityManager em = emf.createEntityManager();
     
@@ -25,7 +30,19 @@ public class SetupTestUsers {
     User admin = new User("admin", "testAdmin");
     User both = new User("user_admin", "testBoth");
 
-    Currency c1 = new Currency("code_c1","name_c1");
+    Currency c1 = em.find(Currency.class,"dkk");
+    
+    
+    
+    Stock s1 = new Stock("AAPL","Apple Inc.");
+    Stock s2 = new Stock("TSLA","Tesla, Inc.");
+    
+    Transaction t1 = new Transaction(s1,100,c1,100.100);
+    Transaction t2 = new Transaction(s2,200,c1,200.200);
+    Transaction t3 = new Transaction(s2,10000,c1,10000.10000);
+    
+    
+    
     if(admin.getUserPass().equals("test")||user.getUserPass().equals("test")||both.getUserPass().equals("test"))
       throw new UnsupportedOperationException("You have not changed the passwords");
 
@@ -39,12 +56,22 @@ public class SetupTestUsers {
     user.setCurrencyCode(c1);
     admin.setCurrencyCode(c1);
     both.setCurrencyCode(c1);
+    
+    user.addTransaction(t1);
+    user.addTransaction(t2);
+    both.addTransaction(t3);
+    
     em.persist(userRole);
     em.persist(adminRole);
+    em.persist(s1);
+    em.persist(s2);
+    em.persist(t1);
+    em.persist(t2);
+    em.persist(t3);
     em.persist(user);
     em.persist(admin);
     em.persist(both);
-    em.persist(c1);
+    
     em.getTransaction().commit();
     System.out.println("PW: " + user.getUserPass());
     System.out.println("Testing user with OK password: " + user.verifyPassword("test"));
